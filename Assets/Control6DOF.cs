92ce5b6ceac169bc977e976ef022e0d782f4949f
bdpfeiffer <bdpfeiffer1@gmail.com>
2020-01-12 15:42:33 -0500

    Added files from Project

---
+++ Assets/Control6DOF.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+public class Control6DOF : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+
+    private MLInputController _controller;
+
+    
+    void Start()
+    {
+        MLInput.Start();
+        _controller = MLInput.GetController(MLInput.Hand.Left);
+        
+        
+    }
+
+    void OnDestroy() {
+        MLInput.Stop();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    
+        transform.position = _controller.Position;
+        transform.rotation = _controller.Orientation;
+
+        Vector3 vector3 = new Vector3(1,0,1);
+
+        
+
+
+    }
+}
---
+++ Assets/TurnAround.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class TurnAround : MonoBehaviour
+{
+
+	public Camera Camera;
+    [Tooltip("Spin: Yes or No")]
+	public bool spin;
+	public float speed = 10f;
+
+	[HideInInspector]
+	public bool clockwise = true;
+	[HideInInspector]
+	public float direction = 1f;
+	[HideInInspector]
+	public float directionChangeSpeed = 2f;
+    // Start is called before the first frame update
+
+	void Start() {
+
+	}
+
+
+    // Update is called once per frame
+   	void Update() {
+
+		
+	updateTransForm();
+		
+	Spin();
+        
+	}
+
+	void Spin() {
+		if (direction < 1f) {
+			direction += Time.deltaTime / (directionChangeSpeed / 2);
+		}
+
+		if (spin) {
+					transform.Rotate(Vector3.up, (speed * direction) * Time.deltaTime);
+                   
+			
+		}
+
+	}
+
+	void updateTransForm() {
+		float speed = Time.deltaTime * 7.0f;
+		Vector3 z = new Vector3(0,0,2);
+		Vector3 pos = Camera.transform.position + Camera.transform.forward + z;
+		transform.position = Vector3.SlerpUnclamped(transform.position,pos,speed);
+		Quaternion rot = Quaternion.LookRotation(transform.position - Camera.transform.position);
+		//transform.rotation = Quaternion.Slerp(transform.rotation,rot,speed);
+	}
+
+}
